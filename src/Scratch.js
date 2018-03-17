627844bd51e92af55a6afef496317e9e39eafd86
SoggyPiggy <soggypiggy@gmail.com>
2018-03-16 22:53:29 -0400

    Using getters and setters now

--- src/Scratch.js
+++ src/Scratch.js
@@ -1,11 +1,25 @@
 module.exports = class Scratch
 {
-	constructor(v = 0)
+	constructor(value = 0)
 	{
-		this.value = v;
+		this._value;
 		this.optimal = false;
 		this.problem = false;
 		this.best = false;
+
+		this.value = value;
+	}
+
+	get value()
+	{
+		return this._value;
+	}
+	set value(v)
+	{
+		let value = Math.round(Number(v));
+		if (value < 0) this._value = 0;
+		if (value > 9) this._value = 9;
+		else this._value = value;
 	}
 
 	reset()
